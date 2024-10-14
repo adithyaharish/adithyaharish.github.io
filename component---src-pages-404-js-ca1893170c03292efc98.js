2e92222963832bfdf1e2ff98a7dc198fa6e3da0e
Adithya Harish <adithyaharish10@gmail.com>
2024-10-14 14:09:00 -0400

    Updates

---
+++ component---src-pages-404-js-ca1893170c03292efc98.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[125],{7412:function(e,t,n){n.r(t);var l=n(6540),o=n(1015),r=n(3673),a=n(6181),c=n(5262),i=n(7859),m=n(1597),u=n(7182),s=n(1378);const f=i.Ay.main`
+  ${e=>{let{theme:t}=e;return t.mixins.flexCenter}};
+  flex-direction: column;
+`,p=i.Ay.h1`
+  color: var(--green);
+  font-family: var(--font-mono);
+  font-size: clamp(100px, 25vw, 200px);
+  line-height: 1;
+`,h=i.Ay.h2`
+  font-size: clamp(30px, 5vw, 50px);
+  font-weight: 400;
+`,E=(0,i.Ay)(o.Link)`
+  ${e=>{let{theme:t}=e;return t.mixins.bigButton}};
+  margin-top: 40px;
+`;t.default=e=>{let{location:t}=e;const{0:n,1:o}=(0,l.useState)(!1),i=(0,s.jt)();(0,l.useEffect)((()=>{if(i)return;const e=setTimeout((()=>o(!0)),m.LF);return()=>clearTimeout(e)}),[]);const g=l.createElement(f,{className:"fillHeight"},l.createElement(p,null,"404"),l.createElement(h,null,"Page Not Found"),l.createElement(E,{to:"/"},"Go Home"));return l.createElement(u.PE,{location:t},l.createElement(r.m,{title:"Page Not Found"}),i?l.createElement(l.Fragment,null,g):l.createElement(a.A,{component:null},n&&l.createElement(c.A,{timeout:500,classNames:"fadeup"},g)))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ca1893170c03292efc98.js.map
---
+++ component---src-pages-index-js-79297272d5b18728846c.js
@@ -0,0 +1,4 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[293],{5608:function(e,t,l){l.r(t);var n=l(6540),c=l(7859),a=l(7182);const r=c.Ay.main`
+  counter-reset: section;
+`;t.default=e=>{let{location:t}=e;return n.createElement(a.PE,{location:t},n.createElement(r,{className:"fillHeight"},n.createElement(a.lq,null),n.createElement(a.Im,null),n.createElement(a.XP,null),n.createElement(a.Jt,null),n.createElement(a.Bv,null)))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-79297272d5b18728846c.js.map
